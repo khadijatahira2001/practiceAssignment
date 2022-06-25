5d0ee520a9b51efad48fbc9be0a351e6c940c0c0
Khadija Tahira <67519765+khadijatahira2001@users.noreply.github.com>
2022-06-25 21:52:52 +0500

    initial commit

---
+++ sum_of_two-nums.py
@@ -0,0 +1,16 @@
+# User defined funtion
+def sum(a, b):
+        print("sum of ", a, b, "is ", a+b)
+
+
+a = int(3.3)
+b = 4
+sum(a, b)
+
+#built in function
+"""
+a = 9
+b = 6
+add = sum((a, b))
+
+print("sum of ", a, b, "is ", add) """

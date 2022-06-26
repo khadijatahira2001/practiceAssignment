4de420fcec37c857ee9ed3ea3b9bd91ee6f73e3c
Khadija Tahira <67519765+khadijatahira2001@users.noreply.github.com>
2022-06-26 13:41:43 +0500

    initial commit

---
+++ product.py
@@ -0,0 +1,16 @@
+def product(a, b):
+    print("product of ", a, " and ", b, " is ", a*b )
+
+
+num1 = input("Enter a number : ")
+print("Number is: " + num1)
+
+num2 = input("Enter any number : ")
+print("Number is: " + num2)
+x = int(num1)
+y = int(num2)
+
+product(x, y)
+
+
+

d8803a024632e8b2f19857d58a0536e3d0dc70db
Khadija Tahira <67519765+khadijatahira2001@users.noreply.github.com>
2022-06-27 22:30:02 +0500

    fIbonacci series

---
+++ Fibonacci.py
@@ -0,0 +1,15 @@
+
+num = int(input("Enter a number "))
+a = int(0)
+b = int(1)
+next = a+b
+print("Fibonacci series")
+print(a)
+print(b)
+i = 3
+for i in range(i, num+1):
+    print(next)
+    a = b
+    b = next
+    next = a + b
+

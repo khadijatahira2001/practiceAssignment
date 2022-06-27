f6e27e0c3ec9bebbc74cb828a338c32085eaffa3
Khadija Tahira <67519765+khadijatahira2001@users.noreply.github.com>
2022-06-27 23:07:43 +0500

    Swap two numbers

---
+++ swap.py
@@ -0,0 +1,11 @@
+num1 = int(input("Enter value for number1 = "))
+num2 = int(input("Enter value for number2 = "))
+
+temp = 0
+temp = num1
+num1 = num2
+num2 = temp
+
+print("number 1 = ", num1)
+print("number 2 = ", num2)
+print("Numbers are swapped successfully")

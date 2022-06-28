d23bd487c3afca9cf47bc2338b7cab1debac54b4
Khadija Tahira <67519765+khadijatahira2001@users.noreply.github.com>
2022-06-28 19:41:42 +0500

    Area and perimeter

---
+++ rectangle.py
@@ -0,0 +1,10 @@
+# Area of rectangle
+l = int(input("Enter length"))
+w = int(input("Enter width"))
+A = l*w
+print("Area of a rectangle = ", A)
+
+# perimeter of a rectangle
+
+P = 2*l+w
+print("Perimeter of a rectangle = ", P)

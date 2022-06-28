951887cf34801df24ffaec0b10c659100166aa01
Khadija Tahira <67519765+khadijatahira2001@users.noreply.github.com>
2022-06-28 21:40:45 +0500

    kmph to mph and vice versa

---
+++ distance_conversion.py
@@ -0,0 +1,13 @@
+# first convert kilometers per hour to miles per hour
+print("****Convert kilometers per hour to miles per hour****")
+print("                                                     ")
+kmph = float(input("Enter distance in kilometer : "))
+mph = kmph * 0.6213712
+print(kmph, "km/hr = ", mph, "miles/hr")
+
+# first convert miles per hour to kilometers per hour
+print("****convert miles per hour to kilometers per hour****")
+print("                                                     ")
+mph = float(input("Enter distance in miles : "))
+kmph = mph * 1.6
+print(mph, "miles/hr = ", kmph, "kilometers/hr")

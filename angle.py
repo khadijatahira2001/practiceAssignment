77fc3366f94d2f26842e4b04d510006386e12b5d
Khadija Tahira <67519765+khadijatahira2001@users.noreply.github.com>
2022-06-28 20:26:27 +0500

    Third angle of triangle

---
+++ angle.py
@@ -0,0 +1,9 @@
+
+# Step 1: add the two angles that you are given together
+# Step 2: subtract your answer from 180
+
+angle1 = int(input("Enter angle A  "))
+angle2 = int(input("Enter angle B  "))
+C = angle1+angle2
+angle3 = 180-C
+print("third angle of a triangle is ", angle3)

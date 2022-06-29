8f9058088ca6c83cabfa95f1f23dffe01b926f8a
Khadija Tahira <67519765+khadijatahira2001@users.noreply.github.com>
2022-06-29 21:58:25 +0500

    initial commit

--- Fibonacci.py
+++ Fibonacci.py
@@ -6,10 +6,14 @@ next = a+b
 print("Fibonacci series")
 print(a)
 print(b)
-i = 3
-for i in range(i, num+1):
-    print(next)
-    a = b
-    b = next
-    next = a + b
-
+def Fibonacci(num):
+    a = int(0)
+    b = int(1)
+    next = a+b
+    i = 3
+    for i in range(i, num+1):
+        print(next)
+        a = b
+        b = next
+        next = a + b
+Fibonacci(num)
